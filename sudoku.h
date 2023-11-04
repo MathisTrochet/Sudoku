9d401cb45e6e07db4454f76a1cadd9dbd5e437ea
MathisTrochet <mathis.trochet23@gmail.com>
2023-11-04 18:29:25 +0100

    ajout des proba dans structCase

--- sudoku.h
+++ sudoku.h
@@ -7,13 +7,22 @@ typedef struct {
     int valeur;
     int posX;
     int posY;
+    int proba_1;
+    int proba_2;
+    int proba_3;
+    int proba_4;
+    int proba_5;
+    int proba_6;
+    int proba_7;
+    int proba_8;
+    int proba_9;
 } *structCase;
 
 typedef struct{
     structCase cellule;
 }grille;
 
-//appel des fonctions utilisées
+//appel des fonctions utilisï¿½es
     structCase **create_grille();
     structCase **add_case(structCase **grille, int val, int posX, int posY);
     structCase **rem_case(structCase **grille, int posX, int posY);
@@ -24,4 +33,6 @@ typedef struct{
     bool verifColonne (structCase **grille, int colonne);
     bool verifCarre (structCase **grille, int x, int y);
     bool verifGrille(structCase **grille);
-#endif // SUDOKU_H_INCLUDED
+    structCase **regle1(structCase **grille);
+
+    #endif // SUDOKU_H_INCLUDED
